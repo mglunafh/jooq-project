d13c9e6857bf070a4cad0e0409628520b971db4d
Fedor Uvarychev <ufispb@gmail.com>
2020-01-30 09:35:42 +0300

    Got rid of complicated work with properties

--- migration/src/main/java/org/some/generic/jooq/migration/Migrator.java
+++ migration/src/main/java/org/some/generic/jooq/migration/Migrator.java
@@ -1,56 +1,27 @@
 package org.some.generic.jooq.migration;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 import javax.sql.DataSource;
 import org.flywaydb.core.Flyway;
 
 
 public class Migrator {
 
-  private static final String PROP_PREFIX = "flyway.sql-migration-prefix";
-  private static final String PROP_LOCATIONS = "flyway.locations";
-  private static final String PROP_SCHEMA = "flyway.schemas";
-  private static final String PROP_TABLE = "flyway.table";
+  private static final String PREFIX = "some-";
+  private static final String LOCATIONS = "classpath:db/migration";
+  private static final String SCHEMA = "some_schema";
+  private static final String TABLE = "schema_version";
 
   private final Flyway flyway;
 
   public Migrator(DataSource dataSource) {
 
-    Properties prop = getValidatedProperties();
     this.flyway = Flyway.configure()
         .dataSource(dataSource)
-        .locations(prop.getProperty(PROP_LOCATIONS))
-        .sqlMigrationPrefix(prop.getProperty(PROP_PREFIX))
-        .schemas(prop.getProperty(PROP_SCHEMA))
-        .table(prop.getProperty(PROP_TABLE))
+        .locations(LOCATIONS)
+        .sqlMigrationPrefix(PREFIX)
+        .schemas(SCHEMA)
+        .table(TABLE)
         .load();
-  }
-
-  private static Properties getValidatedProperties() {
-
-    try (InputStream stream = Migrator.class.getResourceAsStream("migration.properties")) {
-      Properties prop = new Properties();
-      prop.load(stream);
-
-      String[] necessary = {PROP_LOCATIONS, PROP_PREFIX, PROP_SCHEMA, PROP_TABLE};
-      StringBuilder sb = new StringBuilder("The following properties are missing in application.properties: ");
-      boolean missingProperties = false;
-      for (String property : necessary) {
-        if (!prop.containsKey(property)) {
-          missingProperties = true;
-          sb.append("'").append(property).append("' ");
-        }
-      }
-      if (missingProperties) {
-        throw new RuntimeException(sb.toString());
-      }
-      return prop;
-
-    } catch (IOException ex) {
-      throw new RuntimeException(ex);
-    }
   }
 
   public void migrate() {
